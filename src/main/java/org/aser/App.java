47167de1d6f56941cef87782cd3ea5340b091831
khdta <khd.tayeb@gmail.com>
2021-01-18 22:57:23 +0100

    Initial commit

---
+++ src/main/java/org/aser/App.java
@@ -0,0 +1,51 @@
+package org.aser;
+
+
+public class Lesson1 {
+    public static void main (String []args) {
+       /* System.out.println("Hello World");
+        double myFirstVariable = 10.5;
+        System.out.println(myFirstVariable);
+        myFirstVariable = myFirstVariable - 0.5;
+        System.out.println(myFirstVariable);
+
+        System.out.println (myFirstVariable++);
+        System.out.println(myFirstVariable);
+        System.out.println (++myFirstVariable);*/
+
+        /*int x = 10;
+        int y = 20;
+        System.out.println(x<y);*/
+
+       /* int num1=3,num2=6,result;
+        char operator = '+';
+        switch(operator){
+            case '+':
+                result = num1+num2;
+                System.out.println(result);
+                System.out.println("hello");
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println(result);
+                System.out.println("hello from minus");
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println(result);
+                break;
+            case '/':
+                result = num1 / num2;
+                System.out.println(result);
+                break;
+            default:
+                System.out.println("wrong operation");
+
+                }*/
+
+        String name = "Ahmad";
+        String lastName = "Taleb";
+        System.out.println("your full name is: " + name+" "+ lastName);
+
+
+    }
